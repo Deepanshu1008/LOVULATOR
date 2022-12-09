43d3a53c985f49eb1c7a40670f0bc6c980813378
Deepanshu Gupta <89475845+Deepanshu1008@users.noreply.github.com>
2022-12-10 00:56:34 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -3,22 +3,22 @@
 function myFunction() {
   let loveScore = Math.random() * 100;
   loveScore = Math.floor(loveScore) + 1;
-  console.log("kal");
+  
   if (loveScore >= 70) {
     $("#page").html(`<h3 style = "color: purple">Your love score is ${loveScore}% <br>You love each other and be happy</h3>`);
-    console.log("kal");
+    
   }
 
   else if (loveScore < 70 && loveScore > 49) {
     $("#page").html(`<h3 style = "color: purple">Your love score is ${loveScore}% <br>You have nice relations you are good friends</h3>`
     );
-    console.log("kal");
+    
   }
 
 
   else {
     $("#page").html('<h3 style = "color: purple">Your love score is ${loveScore}% <br>RUN AWAY!!!!!</h3>');
-    console.log("kal");
+    
   }
 
 
