ea054c9b3ae5360d77a85786b0f83359479f178f
Deepanshu Gupta <89475845+Deepanshu1008@users.noreply.github.com>
2022-12-10 00:47:11 +0530

    updated jquery and js

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,35 @@
-let userName = prompt(`Please write your name:`);
-let userPartnerName = prompt(`Please write your partner name:`);
 
-let loveScore = Math.random() * 100;
-loveScore = Math.floor(loveScore) + 1;
 
-if (loveScore >= 70) {
-  document.write(
-    `<h3 style = "color: purple">Your love score is ${loveScore}% <br>You love each other and be happy</h3>`
-  );
-} else if (loveScore < 70 && loveScore > 49) {
-  document.write(
-    `<h3 style = "color: green">Your love score is ${loveScore}% <br>You have nice relations you are good friends</h3>`
-  );
-} else {
-  document.write(
-    `<h3 style = "color: red">Your love score is ${loveScore}% <br>RUN AWAY!!!!!</h3>`
-  );
+function myFunction() {
+  let loveScore = Math.random() * 100;
+  loveScore = Math.floor(loveScore) + 1;
+  console.log("kal");
+  if (loveScore >= 70) {
+    $("#page").html(`<h3 style = "color: purple">Your love score is ${loveScore}% <br>You love each other and be happy</h3>`);
+    console.log("kal");
+  }
+
+  else if (loveScore < 70 && loveScore > 49) {
+    $("#page").html(`<h3 style = "color: purple">Your love score is ${loveScore}% <br>You have nice relations you are good friends</h3>`
+    );
+    console.log("kal");
+  }
+
+
+  else {
+    $("#page").html('<h3 style = "color: purple">Your love score is ${loveScore}% <br>RUN AWAY!!!!!</h3>');
+    console.log("kal");
+  }
+
+
+  const h3s = document
+    .querySelector("h3")
+    .setAttribute(
+      "style",
+      "color: purple; font-size: 80px; text-align: center; margin-top:250px;font-family: 'Ewert', cursive; text-shadow: 5px 5px 15px red;"
+    );
+  const bodys = document
+    .querySelector("body")
+    .setAttribute("style", "background-color:#edc9dc;");
+
 }
-
-const h3s = document
-  .querySelector("h3")
-  .setAttribute(
-    "style",
-    "color: #CEE5D0  ; font-size: 80px; text-align: center; margin-top:250px;font-family: 'Ewert', cursive; text-shadow: 5px 5px 15px red;"
-  );
-const bodys = document
-  .querySelector("body")
-  .setAttribute("style", "background-color: #082032;");
